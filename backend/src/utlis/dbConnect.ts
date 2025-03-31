adf1321967f0de3e77a62fe475056274959d4341
SachinSharma-LordHeydes <thereisamailforme@gmail.com>
2025-03-31 09:35:31 +0545

    Update dbConnect.ts

--- backend/src/utlis/dbConnect.ts
+++ backend/src/utlis/dbConnect.ts
@@ -2,7 +2,7 @@ import mongoose, { mongo } from "mongoose";
 
 export const dbConnect=async()=>{
     try {
-        const dbConnectresponse=await mongoose.connect("mongodb+srv://mailitttome:vKLMEV9ZFW8-.za@cluster0.2j2bk.mongodb.net/motuvaii?retryWrites=true&w=majority")
+        const dbConnectresponse=await mongoose.connect("")
         console.log("BD connected Sucessfully")
     } catch (error:any) {
         console.log("Error occured While Connecting to DB",error)
